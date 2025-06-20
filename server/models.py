45d86525d01c937d83903ee99e53b3c819f0c3c4
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 16:02:47 +0300

    Implement full Flask backend with Message model, CRUD routes, CORS, and seeding to support React frontend

--- server/models.py
+++ server/models.py
@@ -1,16 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+from flask_migrate import Migrate
 
-db = SQLAlchemy()
+from models import db, Message
 
-class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String)
-    username = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+CORS(app)
+migrate = Migrate(app, db)
 
-    def __repr__(self):
-        return f'<Message by {self.username}: {self.body[:10]}...>'
+db.init_app(app)
+
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        return make_response(
+            jsonify([message.to_dict() for message in messages]), 200
+        )
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        new
